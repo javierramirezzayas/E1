61bf25aa1d09173cbbe86a1f143d2fb0330cbdc9
javierramirezzayas <javier.ramirez1@upr.edu>
2016-08-21 21:45:50 -0400

    changes

---
+++ src/greeting/GreetinginSpanish.java
@@ -0,0 +1,10 @@
+package greeting;
+
+public class GreetinginSpanish {
+	
+	public static void main(String[] args)
+	{
+		//Display Greeting in Spanish
+		System.out.println("Hola como estan?");
+	}
+}
